3e332214129b320b34cd2a938f41d023fd94de46
junwei890 <junweilee72@gmail.com>
2025-11-15 22:42:06 +0800

    Removed C configs

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,7 +5,7 @@ return {
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
-		opts = { ensure_installed = { "lua_ls", "pylsp", "clangd" } },
+		opts = { ensure_installed = { "lua_ls", "pylsp" } },
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
@@ -20,7 +20,6 @@ return {
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			vim.lsp.enable("lua_ls", { capabilities = capabilities })
 			vim.lsp.enable("pylsp", { capabilities = capabilities })
-			vim.lsp.enable("clangd", { capabilities = capabilities })
 		end,
 	},
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local tsconfig = require("nvim-treesitter.configs")
 		tsconfig.setup({
-			ensure_installed = { "lua", "c", "python" },
+			ensure_installed = { "lua", "python" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
