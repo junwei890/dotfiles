8828d6b860f0f8779dfac21a89f4983d5a8daa9e
junwei890 <junweilee72@gmail.com>
2025-11-17 21:17:28 +0800

    Updated copy script

--- copy.sh
+++ copy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+rm -rf ~/dotfiles/nvim
 cp -r ~/.config/nvim ~/dotfiles/
 
 if [ ! -d ~/dotfiles/tmux ]; then
 	mkdir ~/dotfiles/tmux
 fi
-
 cp ~/.tmux.conf ~/dotfiles/tmux/
 
 stylua ~/dotfiles/nvim
